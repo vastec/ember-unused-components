ab27c19d7f6fa2eccbdfaa9eca4a47a18262fa6c
Tomasz Nieżurawski <tommaqs@gmail.com>
2018-05-17 16:00:10 +0200

    Basic functionality

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-unused-components'
+  name: 'ember-unused-components',
+  includedCommands() {
+    return {
+      unusedComponents: require('./lib/commands/unused')
+    };
+  }
 };
