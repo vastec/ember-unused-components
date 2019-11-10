de1123dc7ea5e10219589a257a5f9184cd252b22
Jeff Keen <jeff@keen.me>
2019-11-09 22:03:35 -0600

    Refactor some methods into their own `object-type` file, add some unit tests for name transformations

---
+++ tests/unit/object-info-test.js
@@ -0,0 +1,71 @@
+import test from 'ava';
+
+const objectInfo = require('../../lib/object-info');
+
+test('it converts component paths to names and types correctly', t => {
+  let config = {
+    addonPaths: ['/node_modules/company_buttons'],
+    includeAddons: true,
+  };
+
+  let nameConversions = {
+    'max-button': [
+      '/system/path/example-app/app/components/max-button.js',
+      '/system/path/example-app/app/components/max-button/component.js',
+      '/system/path/example-app/app/components/max-button/template.hbs',
+      '/system/path/example-app/addon/components/max-button/component.js',
+      '/system/path/example-app/addon/components/max-button/template.hbs',
+      '/system/path/example-app/addon/components/max-button.js',
+      '/system/path/example-app/node_modules/company-buttons/addon/components/max-button/component.js',
+      '/system/path/example-app/node_modules/company-buttons/addon/components/max-button/template.hbs',
+      '/system/path/example-app/node_modules/company-buttons/addon/components/max-button.js',
+    ],
+  };
+
+  Object.keys(nameConversions).forEach(name => {
+    nameConversions[name].forEach(path => {
+      let info = objectInfo.get(config, path);
+      t.is(info.name, name, `${path} should yield correct name`);
+      t.is(info.type, 'component', `${path} should yield correct type`);
+    });
+  });
+});
+
+test('it gets addon info correctly', t => {
+  let config = {
+    addonPaths: ['/node_modules/company-buttons'],
+    includeAddons: true,
+  };
+
+  let withAddon = [
+    '/system/path/example-app/node_modules/company-buttons/addon/components/max-button/component.js',
+    '/system/path/example-app/node_modules/company-buttons/addon/components/max-button/template.hbs',
+    '/system/path/example-app/node_modules/company-buttons/addon/components/max-button.js',
+  ];
+
+  let withoutAddon = [
+    '/system/path/example-app/app/components/max-button.js',
+    '/system/path/example-app/app/components/max-button/component.js',
+    '/system/path/example-app/app/components/max-button/template.hbs',
+    '/system/path/example-app/addon/components/max-button/component.js',
+    '/system/path/example-app/addon/components/max-button/template.hbs',
+    '/system/path/example-app/addon/components/max-button.js',
+  ];
+
+  withAddon.forEach(path => {
+    let info = objectInfo.get(config, path);
+
+    t.is(info.source, 'company-buttons', `${path} should yield correct source name`);
+    t.is(
+      info.sourcePath,
+      '/node_modules/company-buttons',
+      `${path} should yield correct source path`
+    );
+  });
+
+  withoutAddon.forEach(path => {
+    let info = objectInfo.get(config, path);
+    t.is(info.source, undefined, `${path} should not have source name`);
+    t.is(info.sourcePath, undefined, `${path} should not have source path`);
+  });
+});
